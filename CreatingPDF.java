acf5f4f2f07547bdce301d7623ad35d4b981a347
Piyush Joshi <joshipiyush217@gmail.com>
2015-05-03 21:22:41 +0530

    adding code to create pdf from any input file

---
+++ CreatingPDF.java
@@ -0,0 +1,37 @@
+
+
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+
+/**
+
+ */
+public class CreatingPDF {
+
+    public static void main(String[] args) {
+
+        Document document = new Document();
+
+        try {
+            PdfWriter.getInstance(document,
+                new FileOutputStream("HelloWorld.pdf"));
+
+            document.open();
+            document.add(new Paragraph("A Hello World PDF document."));
+            document.close(); // no need to close PDFwriter?
+
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done");
+
+    }
+}
